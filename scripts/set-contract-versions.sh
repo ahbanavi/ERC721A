4c0f68d50fb678d99dbb1714073ac6d7f7b09d23
Amirhossein Banavi <ahbanavi@gmail.com>
2022-04-05 11:45:54 +0430

    add script for setting versions

--- scripts/set-contract-versions.sh
+++ scripts/set-contract-versions.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+shopt -s globstar
 # git checkout main;
-status=$(awk "/ERC721A Contracts Version/ && NR < 3" ./contracts/ERC721A.sol);
 
-if [[ -z $status ]]
-then
-    sed -E "2 i // ERC721A Contracts Version v3.1.0" ./contracts/ERC721A.sol | less
-else
-    sed -E "2s/.*/\/\/ ERC721A Contracts Version v3.1.0/" ./contracts/ERC721A.sol | less
-fi
+version_str="ERC721A Contracts Version";
+new_version=$1;
+for file in contracts/**/*.sol; do
+    line=$(sed -n 2p $file);
+    if [[ $line == *"$version_str"* ]]
+    then
+        sed -i "2s/.*/\/\/ $version_str v$new_version/" $file
+    else
+        sed -i "2 i // $version_str v$new_version" $file
+    fi
+done
